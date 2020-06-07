4087d362d9cf62756242480305cd1e9b696403c2
dougvitor <doug.vitor@gmail.com>
2020-06-06 21:26:10 -0300

    Refactoring Predicate

--- src/br/com/home/lambdas/predicate/PredicadoComposicaoMain.java
+++ src/br/com/home/lambdas/predicate/PredicadoComposicaoMain.java
@@ -6,11 +6,16 @@ public class PredicadoComposicaoMain {
 	
 	public static void main(String[] args) {
 		
-		Predicate<Integer> isPar = num -> num % 2 == 0;
-		Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
-		
-		System.out.println(isPar.and(isTresDigitos).negate().test(124));
-		System.out.println(isPar.or(isTresDigitos).test(123));
+		System.out.println(isPar().and(isTresDigitos()).negate().test(124));
+		System.out.println(isPar().or(isTresDigitos()).test(123));
+	}
+
+	private static Predicate<Integer> isTresDigitos() {
+		return num -> num >= 100 && num <= 999;
+	}
+
+	private static Predicate<Integer> isPar() {
+		return num -> num % 2 == 0;
 	}
 
 }
