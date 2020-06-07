aec8d2999c2b78e540f373bfae0d4f5e240945a5
dougvitor <doug.vitor@gmail.com>
2020-06-06 21:05:51 -0300

    177. Predicate #02 (Composição de Função)

---
+++ src/br/com/home/lambdas/predicate/PredicadoComposicaoMain.java
@@ -0,0 +1,16 @@
+package br.com.home.lambdas.predicate;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicaoMain {
+	
+	public static void main(String[] args) {
+		
+		Predicate<Integer> isPar = num -> num % 2 == 0;
+		Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+		
+		System.out.println(isPar.and(isTresDigitos).negate().test(124));
+		System.out.println(isPar.or(isTresDigitos).test(123));
+	}
+
+}
