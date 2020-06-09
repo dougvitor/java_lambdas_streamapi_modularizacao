c2e21d6e10a9b2ad87040de01ce4c2dda85c07bc
dougvitor <douglas.vitor@gmail.com>
2020-06-08 21:35:49 -0300

    179. Unary Operator

---
+++ src/br/com/home/lambdas/unaryoperator/UnaryOperatorMain.java
@@ -0,0 +1,24 @@
+package br.com.home.lambdas.unaryoperator;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorMain {
+
+	public static void main(String[] args) {
+
+		UnaryOperator<Integer> maisDois = n -> n + 2;
+		
+		UnaryOperator<Integer> vezesDois = n -> n * 2;
+		
+		UnaryOperator<Integer> aoQuadrado = n -> n * n;
+		
+		int resultado = maisDois.andThen(vezesDois).andThen(aoQuadrado).apply(0);
+		
+		int resultadoCompose = aoQuadrado.compose(vezesDois).compose(maisDois).apply(0);
+		
+		System.out.println(resultado);
+		System.out.println(resultadoCompose);
+		
+	}
+
+}
